50b00b64def432eb27fa70b4cb43bc3e5e82e3be
Thangg2hm <thangnguyenlleq1207205@gmail.com>
2024-12-05 09:57:25 +0700

    HN_JPN_KS24A_Nguyen Cong Thang_01.cpp

--- HN_JPN_KS24A_Nguyen Cong Thang_01.cpp
+++ HN_JPN_KS24A_Nguyen Cong Thang_01.cpp
@@ -1,90 +1,160 @@
-#include<stdio.h>
-int main(){
-	int arr[100];
-	int luachon;
-	int sophantu;
-	do{
-		printf("\nMENU\n");
-		printf("1.nhap vao so phan tu va gia tri cho mang\n");
-		printf("2.in ra gia tri cac phan tu trong mang\n");
-		printf("3.tim gia tri nho nhat va lon nhat trong mang\n");
-		printf("4.in ra tong cua tat ca phan tu\n");
-		printf("5.them vao mot phan tu vao cuoi mang\n");
-		printf("6.xoa phan tu tai mot vi tri cu the(nguoi dung nhap vi tri)\n");
-		printf("7.sap xep mang theo thu tu giam dan(bubble sort)\n");
-		printf("8.nhap vao 1 phan tu va tim kiem xem no co trong mang hay khong(linear search)\n");
-		printf("9.in ra toan bo so nguyen to trong mang\n");
-		printf("10.sap xep mang theo thu tu tang dan\n");
-		printf("11.thoat\n");
-	    	printf("moi ban nhap vao lua chon cua minh:");
-	     	scanf("%d",&luachon);
-		switch(luachon){
-			case 1:
-				printf("nhap vao so phan tu cua mang:");
-				scanf("%d",&sophantu);
-				for(int i=0;i<sophantu;i++){
-					printf("arr[%d]=",i);
-					scanf("%d",&arr[i]);
-				}    break;
-				
-			case 2:
-			    printf("gia tri cac phan tu trong mang la:");
-				for(int i=0;i<sophantu;i++){
-					printf("%d,",arr[i]);
-				}	break;
-				
-			case 3:
-				if(sophantu>0){
-					int max=arr[0];
-					for(int i=0;i<sophantu;i++){
-					 if(arr[i]>max){
-					  max=arr[i];
-				       }
-			     	}
-				printf("phan tu lon nhat cua mang la:%d",max);
-		    	}
-				if(sophantu>0){
-					int min=arr[0];
-					for(int i=0;i<sophantu;i++){
-						if(arr[i]<min){
-						min=arr[i];
-			        	}
-			    	} 
-				printf("gia tri nho nhat cua mang la:%d",min);
-					
-				}  break;
-					
-			 case 4: 
-                int tong = 0;
-                 for (int i=0;i<sophantu;i++) {
-                       tong += arr[i];
-               }
-                   printf("Tong cac phan tu: %d\n", tong);
-                   break;
-                   
-               case 5:
-               	int phantu;
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int size = 0;
+    int luaChon, phanTu, chiSo, min, max, tong;
+    
+    do {
+        printf("\nMENU\n");
+        printf("1. Nhap so phan tu va gia tri cho mang\n");
+        printf("2. In ra gia tri cac phan tu trong mang\n");
+        printf("3. Tim gia tri nho nhat va lon nhat trong mang\n");
+        printf("4. In ra tong cua tat ca cac phan tu\n");
+        printf("5. Them mot phan tu vao cuoi mang\n");
+        printf("6. Xoa phan tu tai mot vi tri cu the\n");
+        printf("7. Sap xep mang (Bubble sort)\n");
+        printf("8. Tim kiem phan tu (Linear search)\n");
+        printf("9. In ra tat ca so nguyen to trong mang\n");
+        printf("10. Sap xep mang theo thu tu tang dan (Selection sort)\n");
+        printf("11. Thoat\n");
+        printf("Nhap lua chon cua ban: ");
+        scanf("%d", &luaChon);
+
+        switch (luaChon) {
+            case 1:
+                printf("Nhap so phan tu: ");
+                scanf("%d", &size);
+                for (int i = 0; i < size; i++) {
+                    printf("Nhap phan tu %d: ", i + 1);
+                    scanf("%d", &arr[i]);
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < size; i++) {
+                    printf("arr[%d] = %d ", i, arr[i]);
+                }
+                printf("\n");
+                break;
+
+            case 3:
+                if (size > 0) {
+                    min = arr[0];
+                    max = arr[0];
+                    for (int i = 1; i < size; i++) {
+                        if (arr[i] < min) {
+                            min = arr[i];
+                        }
+                        if (arr[i] > max) {
+                            max = arr[i];
+                        }
+                    }
+                    printf("Gia tri nho nhat: %d\n", min);
+                    printf("Gia tri lon nhat: %d\n", max);
+                }
+                break;
+
+            case 4:
+                tong = 0;
+                for (int i = 0; i < size; i++) {
+                    tong += arr[i];
+                }
+                printf("Tong cac phan tu: %d\n", tong);
+                break;
+
+            case 5:
                 printf("Nhap phan tu can them: ");
-                scanf("%d",&phanTu);
-                arr[sophantu] = phantu;
-                sophantu++;
-                 break;
-             
-                  
-         
-             
-            
-				
-					
-				  
-				   
-		
-				
-		      default:
-		      	printf("ket thuc chuong trinh!")
-		       
-		   
-		
-	} while(luachon!=11);
-	return 0;
+                scanf("%d", &phanTu);
+                arr[size] = phanTu;
+                size++;
+                break;
+
+            case 6:
+                printf("Nhap chi so can xoa: ");
+                scanf("%d", &chiSo);
+                if (chiSo < 0 || chiSo >= size) {
+                    printf("Chi so khong hop le\n");
+                } else {
+                    for (int i = chiSo; i < size - 1; i++) {
+                        arr[i] = arr[i + 1];
+                    }
+                    size--;
+                }
+                break;
+
+            case 7:
+                for (int i = 0; i < size - 1; i++) {
+                    for (int j = 0; j < size - 1 - i; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+                break;
+
+            case 8:
+                printf("Nhap phan tu can tim: ");
+                scanf("%d", &phanTu);
+                chiSo = -1;
+                for (int i = 0; i < size; i++) {
+                    if (arr[i] == phanTu) {
+                        chiSo = i;
+                        break;
+                    }
+                }
+                if (chiSo != -1) {
+                    printf("Phan tu o chi so %d\n", chiSo);
+                } else {
+                    printf("Khong tim thay phan tu\n");
+                }
+                break;
+
+            case 9:
+                for (int i = 0; i < size; i++) {
+                    int laSoNguyenTo = 1;
+                    if (arr[i] < 2) {
+                        laSoNguyenTo = 0;
+                    } else {
+                        for (int j = 2; j <= arr[i] / 2; j++) {
+                            if (arr[i] % j == 0) {
+                                laSoNguyenTo = 0;
+                                break;
+                            }
+                        }
+                    }
+                    if (laSoNguyenTo) {
+                        printf("%d ", arr[i]);
+                    }
+                }
+                printf("\n");
+                break;
+
+            case 10:
+                for (int i = 0; i < size - 1; i++) {
+                    int minIndex = i;
+                    for (int j = i + 1; j < size; j++) {
+                        if (arr[j] < arr[minIndex]) {
+                            minIndex = j;
+                        }
+                    }
+                    int temp = arr[minIndex];
+                    arr[minIndex] = arr[i];
+                    arr[i] = temp;
+                }
+                break;
+
+            case 11:
+                printf("Thoat chuong trinh\n");
+                break;
+
+            default:
+                printf("Lua chon khong hop le\n");
+                break;
+        }
+    } while (luaChon != 11);
+
+    return 0;
 }
